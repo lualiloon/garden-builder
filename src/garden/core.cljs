b09a611fae4207ca65080f374503374c4afd6015
Eliana Medina <emwalker1234@gmail.com>
2023-06-14 13:37:06 -0400

    The Beginning

--- src/garden/core.cljs
+++ src/garden/core.cljs
@@ -4,11 +4,16 @@
 
 ;; --------------- PIXI ------------------
 
-(defonce initial-state {:pixi
-                        {:app
-                         (js/PIXI.Application. #js{:width 640 :height 360
-                                                   :backgroundColor "0xA08D89"})
-                         :sprite (js/PIXI.Sprite.from "images/beet.png")}})
+(defonce initial-state
+  {:pixi
+   {:app
+    (js/PIXI.Application. #js{:width 2048 :height 2048
+                              :backgroundColor "0x4C7C3E"
+                              #_"0x5A3A22"
+                              #_"0xA08D89"})
+    :sprites {:background {}
+              :objects {}
+              :overlays {:grid (js/PIXI.Sprite.from "images/Grid Style B - 2048x2048 - 128x64 - blue.png")}}}})
 
 
 (defonce state-atom
@@ -19,6 +24,21 @@
   []
   (reset! state-atom initial-state))
 
+
+(defn create-herb-sprite
+  [herb-key]
+  (let [file-name (case herb-key
+                    :basil "images/basil.png"
+                    :sage "images/sage.png")]
+    (swap! state-atom
+           assoc-in
+           [:pixi :sprites-objects herb-key]
+           (js/PIXI.Sprite.from file-name))))
+
+
+(defn add-herb-sprite!
+  [herb-key]
+  :TODO)
 
 
 (defn add-pixi
@@ -32,10 +52,19 @@
   (.addChild (.-stage app) sprite))
 
 
+(defn add-all-sprites
+  [app {:keys [background objects overlays] :as all-sprites}]
+  (doseq [sprite (concat (vals background)
+                         (vals objects)
+                         (vals overlays))]
+    (.addChild (.-stage app) sprite)))
+
+
 (defn setup-pixi
-  [pixi-state]
+  [{:keys [app sprites] :as pixi-state}]
   (do (add-pixi pixi-state)
-      (add-sprite pixi-state)))
+      #_(add-sprite pixi-state)
+      (add-all-sprites app sprites)))
 
 
 (defonce _ (setup-pixi (:pixi @state-atom)))
